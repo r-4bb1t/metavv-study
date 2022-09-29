d1890c1075e2ca3122bdeda973e0dac92c4c855c
유영인 <asst2207051@tosspartners.com>
2022-09-30 01:57:28 +0900

    style: image link 스타일링

--- frontend/유영인/next-study/pages/index.tsx
+++ frontend/유영인/next-study/pages/index.tsx
@@ -7,15 +7,19 @@ const Home: NextPage = () => {
   const animalList = ["cat", "dog", "hamster"];
 
   const imageLinkList = animalList.map((animal) => (
-    <Link href={`/${animal}`} key={animal}>
-      <Image alt={animal} src={`/${animal}-1.jpeg`} width={100} height={100} />
-    </Link>
-  ));
-  return (
-    <div className={styles.container}>
-      <>{imageLinkList}</>
+    <div className={styles.link} key={animal}>
+      <Link className={styles.link} href={`/${animal}`}>
+        <Image
+          alt={animal}
+          src={`/${animal}-1.jpeg`}
+          width={200}
+          height={200}
+        />
+      </Link>
     </div>
-  );
+  ));
+
+  return <div className={styles.container}>{imageLinkList}</div>;
 };
 
 export default Home;
